8e2181e5a9cae55c784aa7f90df285e071afc082
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2008-08-15 17:23:40 +0000

    version 0.1.10 from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "div2007"
 #
-# Auto generated 13-06-2008 13:01
+# Auto generated 15-08-2008 19:22
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -11,11 +11,11 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Static Methods for Extensions 2007',
+	'title' => 'Static Methods for Extensions since 2007',
 	'description' => 'This is a transitional version of div which is used by sr_feuser_register, tt_products, sr_email_subscribe, tt_guest and tt_board. NG:typo3.teams.extension-coordination',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.1.8',
+	'version' => '0.1.10',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Extension Coordination Team (ECT)',
-	'author_email' => 'elmar07@googlemail.com',
-	'author_company' => 'Extension Coordination Team, 2007',
+	'author_email' => 'contact@fholzinger.com',
+	'author_company' => 'Extension Coordination Team, 2008',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"73f7";s:10:"README.txt";s:4:"ee2d";s:20:"class.tx_div2007.php";s:4:"38a8";s:26:"class.tx_div2007_alpha.php";s:4:"a16a";s:40:"class.tx_div2007_alpha_language_base.php";s:4:"63e8";s:23:"class.tx_div2007_ff.php";s:4:"fca5";s:12:"ext_icon.gif";s:4:"b4e6";s:14:"doc/manual.sxw";s:4:"1482";s:14:"doc/manual.txt";s:4:"2a5d";s:14:"doc/phpdoc.ini";s:4:"5e47";s:36:"hooks/class.tx_div2007_hooks_cms.php";s:4:"5cd3";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"69ee";s:10:"README.txt";s:4:"ee2d";s:20:"class.tx_div2007.php";s:4:"38a8";s:26:"class.tx_div2007_alpha.php";s:4:"a16a";s:40:"class.tx_div2007_alpha_language_base.php";s:4:"d6b5";s:23:"class.tx_div2007_ff.php";s:4:"fca5";s:12:"ext_icon.gif";s:4:"b4e6";s:14:"doc/manual.sxw";s:4:"1482";s:14:"doc/manual.txt";s:4:"2a5d";s:14:"doc/phpdoc.ini";s:4:"5e47";s:36:"hooks/class.tx_div2007_hooks_cms.php";s:4:"5cd3";s:37:"hooks/class.tx_div2007_hooks_eval.php";s:4:"70ac";}',
 	'suggests' => array(
 	),
 );
