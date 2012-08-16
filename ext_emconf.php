c6c23808ff526d8f15df615d5c4d949d49bd8535
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2012-08-16 12:20:25 +0000

    version 0.10.2 from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "div2007".
 #
-# Auto generated 27-04-2012 16:23
+# Auto generated 16-08-2012 14:19
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This library is continually enhanced. It is used by other extensions. It brings t3lib_div copied from TYPO3 4.7 rc2',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.10.1',
+	'version' => '0.10.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-5.4.99',
+			'php' => '5.2.0-5.3.99',
 			'typo3' => '4.3-4.7.99',
 		),
 		'conflicts' => array(
@@ -46,7 +46,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"14b5";s:10:"README.txt";s:4:"ee2d";s:20:"class.tx_div2007.php";s:4:"dbb6";s:26:"class.tx_div2007_alpha.php";s:4:"03b7";s:27:"class.tx_div2007_alpha5.php";s:4:"fdfa";s:38:"class.tx_div2007_alpha_browse_base.php";s:4:"188c";s:40:"class.tx_div2007_alpha_language_base.php";s:4:"ef16";s:25:"class.tx_div2007_cobj.php";s:4:"7c07";s:35:"class.tx_div2007_configurations.php";s:4:"8a3b";s:28:"class.tx_div2007_context.php";s:4:"8878";s:31:"class.tx_div2007_controller.php";s:4:"57f7";s:24:"class.tx_div2007_div.php";s:4:"73cd";s:26:"class.tx_div2007_email.php";s:4:"b346";s:26:"class.tx_div2007_error.php";s:4:"eaee";s:23:"class.tx_div2007_ff.php";s:4:"0cd7";s:27:"class.tx_div2007_object.php";s:4:"bf34";s:31:"class.tx_div2007_objectbase.php";s:4:"f029";s:31:"class.tx_div2007_parameters.php";s:4:"aa8c";s:34:"class.tx_div2007_selfAwareness.php";s:4:"ffbb";s:29:"class.tx_div2007_t3Loader.php";s:4:"d7e1";s:16:"ext_autoload.php";s:4:"6ddf";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"070c";s:14:"doc/manual.sxw";s:4:"1482";s:14:"doc/manual.txt";s:4:"0cd4";s:14:"doc/phpdoc.ini";s:4:"5e47";s:36:"hooks/class.tx_div2007_hooks_cms.php";s:4:"ba67";s:37:"hooks/class.tx_div2007_hooks_eval.php";s:4:"47a7";s:25:"lang/locallang_common.xml";s:4:"4034";s:42:"spl/class.tx_div2007_spl_arrayIterator.php";s:4:"7b32";s:40:"spl/class.tx_div2007_spl_arrayObject.php";s:4:"a5a4";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"7ce4";s:10:"README.txt";s:4:"ee2d";s:20:"class.tx_div2007.php";s:4:"6bd6";s:26:"class.tx_div2007_alpha.php";s:4:"fcb0";s:27:"class.tx_div2007_alpha5.php";s:4:"b023";s:38:"class.tx_div2007_alpha_browse_base.php";s:4:"1f80";s:40:"class.tx_div2007_alpha_language_base.php";s:4:"622e";s:25:"class.tx_div2007_cobj.php";s:4:"b281";s:35:"class.tx_div2007_configurations.php";s:4:"3921";s:28:"class.tx_div2007_context.php";s:4:"b7c1";s:31:"class.tx_div2007_controller.php";s:4:"8dbc";s:24:"class.tx_div2007_div.php";s:4:"9405";s:26:"class.tx_div2007_email.php";s:4:"0e70";s:26:"class.tx_div2007_error.php";s:4:"eaee";s:23:"class.tx_div2007_ff.php";s:4:"7d3c";s:27:"class.tx_div2007_object.php";s:4:"bf34";s:31:"class.tx_div2007_objectbase.php";s:4:"723d";s:31:"class.tx_div2007_parameters.php";s:4:"9a7b";s:34:"class.tx_div2007_selfAwareness.php";s:4:"ffbb";s:29:"class.tx_div2007_t3Loader.php";s:4:"2528";s:16:"ext_autoload.php";s:4:"6ddf";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"070c";s:14:"doc/manual.sxw";s:4:"1482";s:14:"doc/manual.txt";s:4:"0cd4";s:14:"doc/phpdoc.ini";s:4:"5e47";s:36:"hooks/class.tx_div2007_hooks_cms.php";s:4:"18dc";s:37:"hooks/class.tx_div2007_hooks_eval.php";s:4:"47a7";s:25:"lang/locallang_common.xml";s:4:"4034";s:42:"spl/class.tx_div2007_spl_arrayIterator.php";s:4:"7b32";s:40:"spl/class.tx_div2007_spl_arrayObject.php";s:4:"a5a4";}',
 	'suggests' => array(
 	),
 );
