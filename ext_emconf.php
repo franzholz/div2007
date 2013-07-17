686910969def197e3e16176a3c31faf86af2e5dd
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2013-07-17 07:42:32 +0000

    * fix bug in PHP 5.2 which does not recognize namespace class names like \\TYPO3\\CMS\\Core\\Utility\\VersionNumberUtility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array (
   'description' => 'This library is used by other extensions. It brings t3lib_div copied from TYPO3 4.7.10',
   'category' => 'misc',
   'shy' => 0,
-  'version' => '0.14.0',
+  'version' => '0.14.1',
   'dependencies' => '',
   'conflicts' => '',
   'suggests' =>
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '5.2.0-5.3.99',
-      'typo3' => '4.5.0-6.0.99',
+      'typo3' => '4.5.0-6.1.99',
     ),
     'conflicts' =>
     array (
