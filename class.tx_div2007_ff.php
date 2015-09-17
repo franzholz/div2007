be8cb33799c42e27d2e6f07e5257ab127701090e
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2015-09-17 17:02:19 +0000

    * bug fix: add the static keyword to the function calls of tx_div2007_ff

--- class.tx_div2007_ff.php
+++ class.tx_div2007_ff.php
@@ -46,7 +46,7 @@
  * This class contains diverse static functions to support flexform handling.
  *
  * @package    TYPO3
- * @subpackage div
+ * @subpackage div2007
  * @author     Fabien Udriot <fudriot@omic.ch>
  */
 class tx_div2007_ff {
@@ -56,28 +56,28 @@ class tx_div2007_ff {
 	 *
 	 * @var array
 	 */
-	static $flexForm = array(); //the current loaded flexform
+	static protected $flexForm = array(); //the current loaded flexform
 
 	/**
-	 * A set of flexforms that are stored in case they are going to be use.
+	 * A set of flexforms that are stored in case they are going to be used.
 	 *
 	 * @var array
 	 */
-	static $flexForms = array();
+	static protected $flexForms = array();
 
 	/**
-	 * Load a flexform in memory
+	 * Load a flexform into memory
 	 *
 	 * @param   mixed       $flexForm can be an xml string or an array or a key array.
 	 * @param   string      $flexFormName (optinal) give a name to the flexform in order to be stored for further use.
 	 * @return  void
 	 */
-	function load ($flexForm, $flexFormName = ''){
+	static public function load ($flexForm, $flexFormName = ''){
 		//handle the case $flexForm is a string. It can be a xml string or key array
 		if(is_string($flexForm)){
 			//test if $flexForm already exists in the memory. In this case load the flexform according to its key
-			if(array_key_exists($flexForm,tx_div2007_ff::$flexForms)){
-				self::$flexForm = tx_div2007_ff::$flexForms[$flexForm];
+			if(array_key_exists($flexForm, self::$flexForms)){
+				self::$flexForm = self::$flexForms[$flexForm];
 			}
 			else{
 				//if false, it means it is *still* a string to convert in an array
@@ -102,7 +102,7 @@ class tx_div2007_ff {
 	 * @param   array      the flexForm
 	 * @return  void
 	 */
-	function setFlexForm ($flexFormName, $flexForm){
+	static public function setFlexForm ($flexFormName, $flexForm){
 		self::$flexForms[$flexFormName] = $flexForm;
 	}
 
@@ -112,7 +112,7 @@ class tx_div2007_ff {
 	 * @param   string     the flexForm name
 	 * @return  array      the flexform
 	 */
-	function getFlexForm ($flexFormName) {
+	static public function getFlexForm ($flexFormName) {
 		$result = false;
 		if(array_key_exists($flexFormName, self::$flexForms)) {
 			$result = self::$flexForms[$flexFormName];
@@ -130,18 +130,18 @@ class tx_div2007_ff {
 	 * @param   string     $value Value pointer, eg. "vDEF"
 	 * @return  array      The content.
 	 */
-	function get () {
+	static public function get () {
 
-		//true when the first arguement is a flexForm or a reference to flexForm
+		//true when the first argument is a flexForm or a reference to flexForm
 		if(
 			is_array(func_get_arg(0)) ||
 			array_key_exists(
 				func_get_arg(0),
-				tx_div2007_ff::$flexForms
+				self::$flexForms
 			)
 		) {
 			//case 1, $args 1 is an array...     case 2, $args 1 is a key array that contains a flexform
-			is_array(func_get_arg(0)) ? $_flexForm = func_get_arg(0) : $_flexForm =& tx_div2007_ff::getFlexForm(func_get_arg(0));
+			is_array(func_get_arg(0)) ? $_flexForm = func_get_arg(0) : $_flexForm =& self::getFlexForm(func_get_arg(0));
 			$index = 1;
 		} else {
 			$_flexForm = &self::$flexForm;
@@ -168,7 +168,7 @@ class tx_div2007_ff {
 	 * @param   string     Value for outermost key, typ. "vDEF" depending on language.
 	 * @return  mixed      The value, typ. string. private
 	 */
-	function _getFFValueFromSheetArray ($sheetArray, $fieldNameArr, $value){
+	static public function _getFFValueFromSheetArray ($sheetArray, $fieldNameArr, $value){
 		$tempArr = $sheetArray;
 		foreach($fieldNameArr as $k => $v){
 			if (
