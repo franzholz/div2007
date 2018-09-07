144a6fc28a6a9ea6dd3623b00ca4a675a06d1896
Franz Holzinger <holzinger@ttproducts.de>
2018-09-07 21:30:40 +0200

    * fix for bug with TYPO3 6.2: readd autoloader file ext_autoload.php * This should work with TYPO3 9.3 .

---
+++ Classes/Api/Css.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace JambageCom\Div2007\Api;
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2018 Franz Holzinger (franz@ttproducts.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.HTMLContent.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Part of the div2007 (Static Methods for Extensions since 2007) extension.
+ *
+ * HTML functions
+ *
+ * @author  Franz Holzinger <franz@ttproducts.de>
+ * @maintainer	Franz Holzinger <franz@ttproducts.de>
+ * @package TYPO3
+ * @subpackage div2007
+ *
+ *
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+
+class Css {
+
+    /**
+     * Returns a class-name
+     *
+     * @param   string      The class name
+     * @param   string      type (HTML tag or combination) You can use '-' as the inside separator sign
+     * @return  string      The combined class name
+     * @see pi_getClassName()
+     */
+    public function getClassName ($class, $type = '')
+    {
+        $result = '';
+        if ($type != '') {
+            $result = $type;
+        }
+        $separators = ' -_';
+        $result .= ucwords($class, $separators);
+        
+        $result = preg_replace('/[^A-Za-z0-9]/', '', $result);
+
+        return $result;
+    }
+}
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array (
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods and t3skin images.',
   'category' => 'misc',
   'shy' => 0,
-  'version' => '1.10.8',
+  'version' => '1.10.10',
   'dependencies' => '',
   'conflicts' => '',
   'suggests' => '',
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '5.5.0-7.99.99',
-      'typo3' => '6.2.0-8.99.99',
+      'typo3' => '6.2.0-9.3.99',
     ),
     'conflicts' =>
     array (
