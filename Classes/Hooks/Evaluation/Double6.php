a27f64bf92345efda9cb78b4e5cf82fcc3d9afa9
Franz Holzinger <holzinger@ttproducts.de>
2019-02-20 09:59:09 +0100

    * change hook tx_double6 into JambageCom\Div2007\Hooks\Evaluation\Double6

---
+++ Classes/Hooks/Evaluation/Double6.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace JambageCom\Div2007\Hooks\Evaluation;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Evaluation function for weights in Gramm
+ *
+ * @package    TYPO3
+ * @subpackage div2007
+ * @author     Franz Holzinger <franz@ttproducts.de>
+ */
+
+class Double6 {
+    /**
+    * Evaluation of 'input'-type values based on 'eval' list
+    *
+    * @param	string		Value to evaluate
+    * @param	string		Is-in string
+    * @param	boolean		if TRUE the value is set
+    * @return	string		Modified $value
+    */
+    public function evaluateFieldValue ($value, $is_in, $set)
+    {
+        if ($set) {
+            $theDec = 0;
+            for ($a = strlen($value); $a > 0; $a--) {
+                if (
+                    substr($value, $a - 1, 1) == '.' ||
+                    substr($value, $a - 1, 1) == ','
+                ) {
+                    $theDec = substr($value, $a);
+                    $value = substr($value, 0, $a - 1);
+                    break;
+                }
+            }
+            $theDec = preg_replace('/[^0-9]/', '', $theDec) . '000000';
+            $value = intval(str_replace(' ', '', $value)) . '.' . substr($theDec, 0, 6);
+        }
+        return $value;
+    }
+}
+
