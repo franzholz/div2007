16be00159a7e95322e2039d492163aaed01f5d6e
Franz Holzinger <holzinger@ttproducts.de>
2020-03-25 22:11:25 +0100

    * add new class \JambageCom\Div2007\Database\QueryBuilderApi with a searchWhere method copied from TYPO3 Core. It returns an object \TYPO3\CMS\Core\Database\Query\Expression\CompositeExpression instead of a string.

---
+++ Classes/Database/QueryBuilderApi.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace JambageCom\Div2007\Database;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\Query\QueryHelper;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+
+class QueryBuilderApi {
+    /**
+     * Generates a search where clause based on the input search words (AND operation - all search words must be found in record.)
+     * Example: The $sw is "content management, system" (from an input form) and the $searchFieldList is "bodytext,header" then the output will be ' AND (bodytext LIKE "%content%" OR header LIKE "%content%") AND (bodytext LIKE "%management%" OR header LIKE "%management%") AND (bodytext LIKE "%system%" OR header LIKE "%system%")'
+     *
+     * @param string $searchWords The search words. These will be separated by space and comma.
+     * @param string $searchFieldList The fields to search in
+     * @param string $searchTable The table name you search in (recommended for DBAL compliance. Will be prepended field names as well)
+     * @return \TYPO3\CMS\Core\Database\Query\Expression\CompositeExpression The WHERE clause.
+     */
+    static public function searchWhere ($searchWords, $searchFieldList, $searchTable)
+    {
+        if (!$searchWords) {
+            return null;
+        }
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($searchTable);
+
+        $prefixTableName = $searchTable ? $searchTable . '.' : '';
+
+        $where = $queryBuilder->expr()->andX();
+        $searchFields = explode(',', $searchFieldList);
+        $searchWords = preg_split('/[ ,]/', $searchWords);
+        foreach ($searchWords as $searchWord) {
+            $searchWord = trim($searchWord);
+            if (strlen($searchWord) < 3) {
+                continue;
+            }
+            $searchWordConstraint = $queryBuilder->expr()->orX();
+            $searchWord = $queryBuilder->escapeLikeWildcards($searchWord);
+            foreach ($searchFields as $field) {
+                $searchWordConstraint->add(
+                    $queryBuilder->expr()->like($prefixTableName . $field, $queryBuilder->quote('%' . $searchWord . '%'))
+                );
+            }
+
+            if ($searchWordConstraint->count()) {
+                $where->add($searchWordConstraint);
+            }
+        }
+
+        if ((string) $where === '') {
+            return null;
+        }
+
+        return $where;
+    }
+}
+
+
