ff2e11ec8735295bfc303ca70fd75e9a64e0ed60
Franz Holzinger <holzinger@ttproducts.de>
2021-03-31 19:35:41 +0200

    * remove support for TYPO3 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'Static Methods since 2007',
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods and t3skin images.',
   'category' => 'misc',
-  'version' => '1.11.6',
+  'version' => '1.11.7',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '5.5.0-7.4.99',
-      'typo3' => '6.2.0-10.4.99',
+      'typo3' => '7.6.0-10.4.99',
     ),
     'conflicts' =>
     array (
