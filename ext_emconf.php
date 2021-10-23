03429f479fa38c06cea67bde1e1bffebb1790840
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-23 15:26:11 +0200

    restructuring

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,11 +5,11 @@
 *
 *********************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
   'title' => 'Static Methods since 2007',
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods and t3skin images.',
   'category' => 'misc',
-  'version' => '1.11.8',
+  'version' => '1.11.9',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
@@ -18,19 +18,19 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'franz@ttproducts.de',
   'author_company' => 'jambage.com',
   'constraints' =>
-  array (
+  [
     'depends' =>
-    array (
+    [
       'php' => '5.5.0-7.4.99',
       'typo3' => '7.6.0-10.4.99',
-    ),
+    ],
     'conflicts' =>
-    array (
-    ),
+    [
+    ],
     'suggests' =>
-    array (
+    [
       'migration_core' => '0.0.1-0.99.99',
-    ),
-  )
-);
+    ],
+  ]
+];
 
