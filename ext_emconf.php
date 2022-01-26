1d7f1964bda66ad3549534344053b0a147bab634
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-26 16:47:24 +0100

    * bugfix: \JambageCom\Div2007\Utility\TcaUtility::removeField has 2 array parameters. * It should still work with PHP 7.4 and under TYPO3 10.4.

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
   'title' => 'Static Methods since 2007',
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods and t3skin images.',
   'category' => 'misc',
-  'version' => '1.13.1',
+  'version' => '1.13.2',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
   [
     'depends' =>
     [
-      'php' => '8.0.0-8.1.99',
+      'php' => '7.4.0-8.1.99',
       'typo3' => '11.5.0-11.5.99',
     ],
     'conflicts' =>
