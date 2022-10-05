40475152858f32bca52e369c27eaac67a773eab3
Franz Holzinger <franz.holzinger@festwein.de>
2022-10-05 21:46:04 +0200

    undetermined execution order of file ext_localconf.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,10 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+if (!defined ('DIV2007_EXT')) {
+    define('DIV2007_EXT', 'div2007');
+}
+
 $emClass = '\\TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility';
 
 if (class_exists($emClass)) {
