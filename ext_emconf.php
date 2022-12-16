abdabaa7e26255a23028b04fff8d33b111330782
Franz Holzinger <franz.holzinger@festwein.de>
2022-12-16 10:08:44 +0100

    TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
   'title' => 'Static Methods since 2007',
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods and t3skin images.',
   'category' => 'misc',
-  'version' => '1.15.2',
+  'version' => '1.16.0',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'depends' =>
     [
       'php' => '7.4.0-8.1.99',
-      'typo3' => '10.4.0-11.5.99',
+      'typo3' => '10.4.0-12.5.99',
     ],
     'conflicts' =>
     [
