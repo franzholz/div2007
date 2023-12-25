2351bae1f02abb0b8ed3804fafbebcf454efb581
Franz Holzinger <franz.holzinger@festwein.de>
2023-12-25 15:22:09 +0100

    remove unused constants and PATH_FE_DIV2007_REL

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,44 +1,29 @@
 <?php
 defined('TYPO3') || defined('TYPO3_MODE') || die('Access denied.');
 
-if (!defined ('DIV2007_EXT')) {
-    define('DIV2007_EXT', 'div2007');
-}
+call_user_func(function ($extensionKey): void {
+    if (!defined ('DIV2007_EXT')) {
+        define('DIV2007_EXT', $extensionKey);
+    }
 
-call_user_func(function () {
     $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
         \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-    )->get(DIV2007_EXT);
+    )->get($extensionKey);
 
     if (
-        isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT]) &&
-        is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT])
+        isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]) &&
+        is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey])
     ) {
-        $storeArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT];
+        $storeArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey];
     } else {
         unset($storeArray);
     }
 
     if (isset($extensionConfiguration) && is_array($extensionConfiguration)) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT] = $extensionConfiguration;
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey] = $extensionConfiguration;
         if (isset($storeArray) && is_array($storeArray)) {
-            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT] = array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][DIV2007_EXT], $storeArray);
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey] = array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey], $storeArray);
         }
-    }
-
-    $emClass = '\\TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility';
-    $extensionPath = call_user_func($emClass . '::extPath', DIV2007_EXT);
-
-    if (!defined ('PATH_BE_DIV2007')) {
-        define('PATH_BE_DIV2007', $extensionPath);
-    }
-
-    if (!defined ('PATH_FE_DIV2007_REL')) {
-        $relativeExtensionPath = \TYPO3\CMS\Core\Utility\PathUtility::stripPathSitePrefix(
-            $extensionPath
-        );
-
-        define('PATH_FE_DIV2007_REL', $relativeExtensionPath);
     }
 
     if (!defined ('STATIC_INFO_TABLES_EXT')) {
@@ -59,5 +44,5 @@ call_user_func(function () {
     if (!defined('DIV2007_ICONS_SUBPATH')) {
         define('DIV2007_ICONS_SUBPATH', 'Resources/Public/Images/Icons/');
     }
-});
+}, 'div2007');
 
