2ac2c199a4f24c0fd1cd4160422d02bd1e57794a
Franz Holzinger <franz.holzinger@festwein.de>
2024-02-27 18:02:16 +0100

    version 2.1.0 requires TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,18 +6,18 @@ $EM_CONF[$_EXTKEY] = [
   'title' => 'Static Methods since 2007',
   'description' => 'This library offers classes and functions to other TYPO3 extensions. It provides a modified t3lib_div of TYPO3 4.7.10. Replacement for tslib_pibase methods.',
   'category' => 'misc',
-  'version' => '2.0.1',
+  'version' => '2.1.0',
   'state' => 'stable',
   'author' => 'Franz Holzinger',
   'author_email' => 'franz@ttproducts.de',
   'author_company' => 'jambage.com',
   'constraints' => [
     'depends' => [
-      'php' => '7.4.0-8.4.99',
-      'typo3' => '10.4.0-12.4.99',
+      'php' => '8.0.0-8.4.99',
+      'typo3' => '12.4.0-12.4.99',
     ],
     'suggests' => [
-        'typo3db_legacy' => '1.0.0-1.2.99',
+        'typo3db_legacy' => '1.2.0-1.2.99',
     ],
     'conflicts' => [
     ],
