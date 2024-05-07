73fb3eea39442f884111aceda2d9b0012209901f
Franz Holzinger <franz.holzinger@festwein.de>
2024-05-07 18:10:35 +0200

    remove constant TYPO3_MODE finally

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3') || defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 call_user_func(function ($extensionKey, $table): void {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3') || defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 call_user_func(function ($extensionKey): void {
     if (!defined('DIV2007_EXT')) {
