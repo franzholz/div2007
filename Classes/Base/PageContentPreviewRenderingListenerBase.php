99ba74a61d82dd2dfd44574991eed5468c9dcd60
Franz Holzinger <franz.holzinger@festwein.de>
2024-06-14 21:43:19 +0200

    fix error with not working container extension

--- Classes/Base/PageContentPreviewRenderingListenerBase.php
+++ Classes/Base/PageContentPreviewRenderingListenerBase.php
@@ -3,30 +3,30 @@
 namespace JambageCom\Div2007\Base;
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2018 Franz Holzinger <franz@ttproducts.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008-2018 Franz Holzinger <franz@ttproducts.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 /**
  * Part of the div2007 (Static Methods for Extensions End of 2007) extension.
  *
@@ -55,7 +55,9 @@ class PageContentPreviewRenderingListenerBase implements SingletonInterface
         $pageContext = $event->getPageLayoutContext();
         $pageRecord = $pageContext->getPageRecord();
         $codes = $this->pmDrawItem($record, $pageRecord);
-        $event->setPreviewContent($content . $codes);
+        if (strlen($content . $codes)) {
+            $event->setPreviewContent($content . $codes);
+        }
     }
 
     /**
@@ -64,9 +66,9 @@ class PageContentPreviewRenderingListenerBase implements SingletonInterface
      * @param	array		record
      * @param	object		the parent object
      */
-    public function pmDrawItem(array $record, array $pageRecord): string
+    public function pmDrawItem(array $record, array $pageRecord): ?string
     {
-        $codes = '';
+        $codes = null;
         $extensionKey = '';
         if (
             $this->extensionKey != ''
@@ -79,7 +81,7 @@ class PageContentPreviewRenderingListenerBase implements SingletonInterface
             ExtensionManagementUtility::isLoaded($extensionKey) &&
             in_array(
                 intval($pageRecord['doktype']),
-                [1, 2, 5]
+                     [1, 2, 5]
             ) &&
             $record['pi_flexform'] != ''
         ) {
@@ -88,10 +90,10 @@ class PageContentPreviewRenderingListenerBase implements SingletonInterface
                 $extensionKey
             );
             $codes =
-                'CODE: ' . FlexformUtility::get(
-                    $extensionKey,
-                    'display_mode'
-                );
+            'CODE: ' . FlexformUtility::get(
+                $extensionKey,
+                'display_mode'
+            );
         }
 
         return $codes;
