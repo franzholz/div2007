14ddfd56a3f5dd529e11b44f6ec6f82c9e7fe103
Franz Holzinger <franz.holzinger@festwein.de>
2026-03-14 18:57:47 +0100

    support PHP 8.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
   'author_company' => 'jambage.com',
   'constraints' => [
     'depends' => [
-      'php' => '8.0.0-8.4.99',
+      'php' => '8.2.0-8.5.99',
       'typo3' => '12.4.0-13.4.99',
     ],
     'suggests' => [
