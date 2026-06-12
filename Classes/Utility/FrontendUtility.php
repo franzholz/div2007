1bbc6597d4182f2ff235e21f0d2ad541da5a34f6
Franz Holzinger <franz.holzinger@festwein.de>
2026-06-12 16:44:39 +0200

    PathUtility::stripPathSitePrefix only for full file paths

--- Classes/Utility/FrontendUtility.php
+++ Classes/Utility/FrontendUtility.php
@@ -1310,8 +1310,12 @@ class FrontendUtility
                 $xhtmlFix = HtmlUtility::generateXhtmlFix();
                 $imgFile = $incFile;
                 $imgInfo = @getimagesize($imgFile);
+
+                if (!$sanitize) {
+                    $imgFile = PathUtility::stripPathSitePrefix($imgFile));
+                }
                 if (($tsfe ?? null) instanceof TypoScriptFrontendController) {
-                    $result = '<img src="' . htmlspecialchars($tsfe->absRefPrefix . PathUtility::stripPathSitePrefix($imgFile)) . '" width="' . (int)$imgInfo[0] . '" height="' . (int)$imgInfo[1] . '"' . static::getBorderAttribute(' border="0"') . ' ' . $addParams . ' ' . $xhtmlFix . '>';
+                    $result = '<img src="' . htmlspecialchars($tsfe->absRefPrefix . $imgFile . '" width="' . (int)$imgInfo[0] . '" height="' . (int)$imgInfo[1] . '"' . static::getBorderAttribute(' border="0"') . ' ' . $addParams . ' ' . $xhtmlFix . '>';
                 }
             } elseif (filesize($incFile) < 1024 * 1024) {
                 $result = file_get_contents($incFile);
