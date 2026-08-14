66d4c689b0127426253b4d91b1b31b8261b4f1d9
Franz Holzinger <franz.holzinger@festwein.de>
2026-08-14 15:15:53 +0200

    remove dependency on typo3db_legacy

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,9 +16,6 @@ $EM_CONF[$_EXTKEY] = [
       'php' => '8.2.0-8.5.99',
       'typo3' => '13.4.0-14.3.99',
     ],
-    'suggests' => [
-        'typo3db_legacy' => '1.2.0-1.3.99',
-    ],
     'conflicts' => [
     ],
   ],
